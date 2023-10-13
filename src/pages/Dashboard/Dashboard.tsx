d3e3ee0ca6818118fc849c15cd36d4efe3cf49ad
LichSkeleton <lichskeleton1@gmail.com>
2023-10-13 13:46:18 +0300

    Remade 1 lesson

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+  <div>
+    This is the Dashboard component.
+  </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/Login/Login';
+
+const Main: React.FC = () => {
+  return (
+  <div>
+    <h1>This is the Main component.</h1>
+    <Login />
+  </div>
+  );
+};
+
+export default Main;
