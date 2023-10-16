bc061cfc27bc755d80261b4ba602a1b5ad656007
LichSkeleton <lichskeleton1@gmail.com>
2023-10-16 09:55:40 +0300

    lesson 4

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -4,3 +4,5 @@ import { axiosInstance } from "../instance";
 import { ILoginRequest, ILoginResponse } from "./types"
 
 export const login = (params: ILoginRequest): AxiosPromise<ILoginResponse> => axiosInstance.post(Endpoints.AUTH.LOGIN, params)
+
+export const logout = (): AxiosPromise => axiosInstance.get(Endpoints.AUTH.LOGOUT)
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useSelector } from "react-redux";
+import { IRootState, useAppDispatch } from "../../store";
+import { logoutUser } from "../../store/auth/actionCreators";
 import Login from './components/Login/Login';
 
 const Main: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const isLoggedIn = useSelector(
+    (state: IRootState) => !!state.auth.authData.accessToken
+  );
+
+  const renderProfile = () => (
+    <div>
+      <div>Ви успушно авторизувались</div>
+      <button onClick={() => dispatch(logoutUser())}>Logout</button>
+    </div>
+  );
+
   return (
   <div>
     <h1>This is the Main component.</h1>
-    <Login />
+      {isLoggedIn ? renderProfile() : <Login />}
   </div>
   );
 };
